188266f15cfbc6ca423b5e16ec264422d7ca7b07
mamettdd <mamettdd@gmail.com>
2025-10-13 23:38:07 +0700

    Create three_verification.sh

---
+++ Soal_16/three_verification.sh
@@ -0,0 +1,9 @@
+dig @10.70.3.3 lindon.k13.com +short
+dig @10.70.3.3 static.k13.com +short
+
+dig lindon.k13.com +short
+dig static.k13.com +short
+
+sleep 35
+dig lindon.k13.com +short
+dig static.k13.com +short
